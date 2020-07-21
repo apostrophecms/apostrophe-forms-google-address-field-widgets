00c48df7f13dee7a768b3275c061cccffb5b718c
Anthony Tarlao <anthony.tarlao@gmail.com>
2020-07-21 18:36:15 +0200

    Split address

--- index.js
+++ index.js
@@ -3,134 +3,168 @@ const countries = require('./countries');
 module.exports = {
   extend: 'apostrophe-forms-base-field-widgets',
   label: 'Google Address',
-  addFields: [
-    {
-      name: 'googleApiKey',
-      label: 'Google API key',
-      type: 'string',
-      required: true
-    },
-    {
-      name: 'placeholder',
-      label: 'Placeholder',
-      type: 'string',
-      help: 'Text to display in the field before someone uses it (e.g., to provide additional directions).'
-    },
-    {
-      name: 'types',
-      label: 'Google Places Autocomplete Type',
-      type: 'select',
-      htmlHelp: '<a href="https://developers.google.com/maps/documentation/javascript/places-autocomplete#add_autocomplete" target="_blank">More information here</a>',
-      choices: [
-        {
-          value: '',
-          label: 'All'
-        }, {
-          value: 'geocode',
-          label: 'Geocode'
-        }, {
-          value: 'address',
-          label: 'Address'
-        }, {
-          value: 'establishment',
-          label: 'Establishment'
-        }, {
-          value: '(regions)',
-          label: 'Regions'
-        }, {
-          value: '(cities)',
-          label: 'Cities'
-        }
-      ]
-    },
-    {
-      name: 'origin',
-      label: 'Origin',
-      type: 'boolean',
-      htmlHelp: 'The origin point from which location from which the distance to a place result is calculated. <a href="https://developers.google.com/maps/documentation/javascript/places-autocomplete#add_autocomplete" target="_blank">More information here</a>',
-      choices: [
-        {
-          value: true,
-          showFields: [
-            'originLatitude', 'originLongitude'
-          ]
-        }
-      ]
-    },
-    {
-      name: 'originLatitude',
-      label: 'Origin Latitude',
-      type: 'float',
-      required: true
-    },
-    {
-      name: 'originLongitude',
-      label: 'Origin Longitude',
-      type: 'float',
-      required: true
-    },
-    {
-      name: 'bounds',
-      label: 'Bounds',
-      type: 'boolean',
-      htmlHelp: 'The area in which to search for places. <a href="https://developers.google.com/maps/documentation/javascript/places-autocomplete#add_autocomplete" target="_blank">More information here</a>',
-      choices: [
-        {
-          value: true,
-          showFields: [
-            'strictBounds', 'northEastLatitude', 'northEastLongitude', 'southWestLatitude', 'southWestLongitude'
-          ]
-        }
-      ]
-    },
-    {
-      name: 'northEastLatitude',
-      label: 'North East Latitude Bound',
-      type: 'float',
-      required: true
-    },
-    {
-      name: 'northEastLongitude',
-      label: 'North East Longitude Bound',
-      type: 'float',
-      required: true
-    },
-    {
-      name: 'southWestLatitude',
-      label: 'South West Latitude Bound',
-      type: 'float',
-      required: true
-    },
-    {
-      name: 'southWestLongitude',
-      label: 'South West Longitude Bound',
-      type: 'float',
-      required: true
-    },
-    {
-      name: 'strictBounds',
-      label: 'Strict Bounds',
-      type: 'boolean',
-      htmlHelp: 'Returns only those places that are strictly within the region defined by the given bounds. <a href="https://developers.google.com/maps/documentation/javascript/places-autocomplete#add_autocomplete" target="_blank">More information here</a>'
-    },
-    {
-      name: 'countries',
-      label: 'Countries',
-      type: 'array',
-      titleField: 'country',
-      limit: 5,
-      help: 'Restrict search to one or multiple countries (5 at max due to Google restriction). Might be not compatible with origin or bounds values.',
-      schema: [
-        {
-          name: 'country',
-          label: 'Country',
-          type: 'select',
-          choices: countries
-        }
-      ]
-    }
-  ],
   beforeConstruct (self, options) {
+    options.addFields = [
+      {
+        name: 'placeholder',
+        label: 'Placeholder',
+        type: 'string',
+        help: 'Text to display in the field before someone uses it (e.g., to provide additional directions).'
+      },
+      {
+        name: 'types',
+        label: 'Google Places Autocomplete Type',
+        type: 'select',
+        htmlHelp: '<a href="https://developers.google.com/maps/documentation/javascript/places-autocomplete#add_autocomplete" target="_blank">More information here</a>',
+        choices: [
+          {
+            value: '',
+            label: 'All'
+          }, {
+            value: 'geocode',
+            label: 'Geocode'
+          }, {
+            value: 'address',
+            label: 'Address'
+          }, {
+            value: 'establishment',
+            label: 'Establishment'
+          }, {
+            value: '(regions)',
+            label: 'Regions'
+          }, {
+            value: '(cities)',
+            label: 'Cities'
+          }
+        ]
+      },
+      {
+        name: 'origin',
+        label: 'Origin',
+        type: 'boolean',
+        htmlHelp: 'The origin point from which location from which the distance to a place result is calculated. <a href="https://developers.google.com/maps/documentation/javascript/places-autocomplete#add_autocomplete" target="_blank">More information here</a>',
+        choices: [
+          {
+            value: true,
+            showFields: [
+              'originLatitude', 'originLongitude'
+            ]
+          }
+        ]
+      },
+      {
+        name: 'originLatitude',
+        label: 'Origin Latitude',
+        type: 'float',
+        required: true
+      },
+      {
+        name: 'originLongitude',
+        label: 'Origin Longitude',
+        type: 'float',
+        required: true
+      },
+      {
+        name: 'bounds',
+        label: 'Bounds',
+        type: 'boolean',
+        htmlHelp: 'The area in which to search for places. <a href="https://developers.google.com/maps/documentation/javascript/places-autocomplete#add_autocomplete" target="_blank">More information here</a>',
+        choices: [
+          {
+            value: true,
+            showFields: [
+              'strictBounds', 'northEastLatitude', 'northEastLongitude', 'southWestLatitude', 'southWestLongitude'
+            ]
+          }
+        ]
+      },
+      {
+        name: 'northEastLatitude',
+        label: 'North East Latitude Bound',
+        type: 'float',
+        required: true
+      },
+      {
+        name: 'northEastLongitude',
+        label: 'North East Longitude Bound',
+        type: 'float',
+        required: true
+      },
+      {
+        name: 'southWestLatitude',
+        label: 'South West Latitude Bound',
+        type: 'float',
+        required: true
+      },
+      {
+        name: 'southWestLongitude',
+        label: 'South West Longitude Bound',
+        type: 'float',
+        required: true
+      },
+      {
+        name: 'strictBounds',
+        label: 'Strict Bounds',
+        type: 'boolean',
+        htmlHelp: 'Returns only those places that are strictly within the region defined by the given bounds. <a href="https://developers.google.com/maps/documentation/javascript/places-autocomplete#add_autocomplete" target="_blank">More information here</a>'
+      },
+      {
+        name: 'countries',
+        label: 'Countries',
+        type: 'array',
+        titleField: 'country',
+        limit: 5,
+        help: 'Restrict search to one or multiple countries (5 at max due to Google restriction). Might be not compatible with origin or bounds values.',
+        schema: [
+          {
+            name: 'country',
+            label: 'Country',
+            type: 'select',
+            choices: countries
+          }
+        ]
+      }, {
+        name: 'splitAddress',
+        label: 'Split Address',
+        type: 'checkboxes',
+        help: 'Optional: choose fields to split address to',
+        choices: [
+          {
+            value: 'street_number',
+            label: 'Street Number'
+          },
+          {
+            value: 'route',
+            label: 'Route'
+          },
+          {
+            value: 'locality',
+            label: 'City'
+          },
+          {
+            value: 'sublocality_level_1',
+            label: 'City (for NYC area)'
+          },
+          {
+            value: 'administrative_area_level_1',
+            label: 'State'
+          },
+          {
+            value: 'country',
+            label: 'Country'
+          },
+          {
+            value: 'postal_code',
+            label: 'Postal Code'
+          },
+          {
+            value: 'postal_town',
+            label: 'Postal Code (for UK and Sweden)'
+          }
+        ]
+      }
+    ].concat(options.addFields || []);
+
     options.arrangeFields = [
       {
         name: 'settings',
@@ -157,9 +191,16 @@ module.exports = {
           'southWestLatitude',
           'southWestLongitude',
           'strictBounds',
-          'countries'
+          'countries',
+          'splitAddress'
         ]
       }
     ].concat(options.arrangeFields || []);
+  },
+  construct (self, options) {
+    self.on('apostrophe-pages:beforeSend', 'addGoogleApiKey');
+    self.addGoogleApiKey = function (req) {
+      req.data.googleApiKey = options.googleApiKey;
+    };
   }
 };
--- public/js/lean.js
+++ public/js/lean.js
@@ -1,41 +1,74 @@
 apos.utils.widgetPlayers['apostrophe-forms-google-address-field'] = function(el, widget, options) {
-  var scriptSrc = 'https://maps.googleapis.com/maps/api/js?key=' + widget.googleApiKey + '&libraries=places';
-  var alreadyLoaded = document.querySelector('script[src="' + scriptSrc + '"]');
-  if (!alreadyLoaded && widget.googleApiKey) {
-    var googleScript = document.createElement('script');
-    googleScript.setAttribute('src', scriptSrc);
-    document.head.appendChild(googleScript);
-  }
-
-  var input = el.querySelector('input[name=' + widget.fieldName + ']');
-  input.addEventListener('input', function () {
-    var google = window.google;
-
-    var countries = _.map(widget.countries, function (countryObject) {
-      return countryObject.country;
-    });
-
-    var searchOptions = {
-      componentRestrictions: { country: countries }
-    };
-
-    if (widget.types) {
-      searchOptions.types = [widget.types];
+  var googleApiKey = el.querySelector('[data-apos-forms-google-api-key]').dataset.aposFormsGoogleApiKey;
+  if (googleApiKey) {
+    var scriptSrc = 'https://maps.googleapis.com/maps/api/js?key=' + googleApiKey + '&libraries=places';
+    var alreadyLoaded = document.querySelector('script[src="' + scriptSrc + '"]');
+    if (!alreadyLoaded) {
+      var googleScript = document.createElement('script');
+      googleScript.setAttribute('src', scriptSrc);
+      document.head.appendChild(googleScript);
     }
-    if (widget.origin) {
-      searchOptions.origin = new google.maps.LatLng(widget.originLatitude, widget.originLongitude);
-    }
-    if (widget.bounds) {
-      searchOptions.bounds = new google.maps.LatLngBounds(
-        new google.maps.LatLng(widget.northEastLatitude, widget.northEastLongitude),
-        new google.maps.LatLng(widget.southWestLatitude, widget.southWestLongitude)
-      );
 
-      if (widget.strictBounds) {
-        searchOptions.strictBounds = true;
+    var input = el.querySelector('input[name=' + widget.fieldName + ']');
+    input.addEventListener('input', function () {
+      var google = window.google;
+
+      var countries = _.map(widget.countries, function (countryObject) {
+        return countryObject.country;
+      });
+
+      var searchOptions = {
+        componentRestrictions: { country: countries }
+      };
+
+      if (widget.types) {
+        searchOptions.types = [widget.types];
       }
-    }
 
-    new google.maps.places.Autocomplete(input, searchOptions); // eslint-disable-line no-new
-  });
+      if (widget.origin) {
+        searchOptions.origin = new google.maps.LatLng(widget.originLatitude, widget.originLongitude);
+      }
+
+      if (widget.bounds) {
+        searchOptions.bounds = new google.maps.LatLngBounds(
+          new google.maps.LatLng(widget.northEastLatitude, widget.northEastLongitude),
+          new google.maps.LatLng(widget.southWestLatitude, widget.southWestLongitude)
+        );
+
+        if (widget.strictBounds) {
+          searchOptions.strictBounds = true;
+        }
+      }
+
+      var autocomplete = new google.maps.places.Autocomplete(input, searchOptions); // eslint-disable-line no-new
+      autocomplete.setFields(['address_component']);
+
+      if (widget.splitAddress) {
+        autocomplete.addListener('place_changed', function () {
+          var split = widget.splitAddress;
+          var place = autocomplete.getPlace();
+
+          if (place.address_components) {
+            for (var i = 0; i < split.length; i++) {
+              var element = el.querySelector('.apos-forms-input-' + split[i]);
+              element.value = '';
+
+              // Get each component of the address from the place details,
+              // and then fill-in the corresponding field on the form.
+              for (var j = 0; j < place.address_components.length; j++) {
+                var addressType = place.address_components[j].types[0];
+                if (split[i] === addressType) {
+                  element.value = place.address_components[j].long_name;
+                  break;
+                }
+              }
+            }
+          }
+        });
+      }
+    });
+  } else {
+    // eslint-disable-next-line no-console
+    console.error('apostrophe-forms-google-address-field-widgets error: missing Google API key');
+  }
 };
