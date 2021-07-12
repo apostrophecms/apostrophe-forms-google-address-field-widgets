6146986e1eaf4af552487d1f79ab8f5752857d30
Jed <vjeudy@protonmail.com>
2021-07-12 18:02:45 +0200

    uses self.load to set google api key to allow overrides

--- index.js
+++ index.js
@@ -12,6 +12,23 @@ module.exports = {
   },
 
   construct (self, options) {
+    const superLoad = self.load;
+    self.load = function(req, widgets, callback) {
+      return superLoad(req, widgets, function(err) {
+        if (err) {
+          return callback(err);
+        }
+
+        self.addGoogleApiKey(req);
+
+        return callback(null);
+      });
+    };
+
+    self.addGoogleApiKey = function (req) {
+      req.data.googleApiKey = options.googleApiKey;
+    };
+
     self.sanitizeFormField = function (req, form, widget, input, output) {
       const parts = Object.keys(widget.addressParts);
       if (widget.splitAddress && parts && parts.length) {
